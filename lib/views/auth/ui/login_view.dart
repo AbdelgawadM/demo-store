88b9db00ac5b16b8b09653c93a532587e1dadac3
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-10 06:37:31 +0300

    finish refactored login

--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -15,46 +15,134 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            Text(
-              'Welcome to our Market',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 24),
-            Card(
-              margin: EdgeInsets.all(24),
-              color: AppColors.kWhiteColor,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadiusGeometry.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    CustomTextField(
-                      label: 'Email',
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(height: 24),
-                    CustomTextField(
-                      isHidden: isHidden,
-                      label: 'Password',
-                      keyboardType: TextInputType.visiblePassword,
-                      sufIcon: IconButton(
-                        onPressed: () {
-                          isHidden = !isHidden;
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.visibility_off),
-                      ),
-                    ),
-                  ],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 24),
+                Text(
+                  'Welcome to our Market',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              ),
+                const SizedBox(height: 24),
+                Card(
+                  margin: EdgeInsets.all(24),
+                  color: AppColors.kWhiteColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(16),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      spacing: 20,
+                      children: [
+                        CustomTextField(
+                          label: 'Email',
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        CustomTextField(
+                          isHidden: isHidden,
+                          label: 'Password',
+                          keyboardType: TextInputType.visiblePassword,
+                          sufIcon: IconButton(
+                            onPressed: () {
+                              isHidden = !isHidden;
+                              setState(() {});
+                            },
+                            icon: Icon(Icons.visibility_off),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            CustomTextBtn(
+                              text: 'Forget Password?',
+                              onTap: () {},
+                            ),
+                          ],
+                        ),
+                        CustomRow(data: 'Login', onPressed: () {}),
+                        CustomRow(data: 'Login With Google', onPressed: () {}),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Already have Account ?',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 5),
+                            CustomTextBtn(text: 'Sign up'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomRow extends StatelessWidget {
+  const CustomRow({super.key, this.onPressed, required this.data});
+  final void Function()? onPressed;
+  final String data;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(data, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        CustomArrowBtn(onPressed: onPressed),
+      ],
+    );
+  }
+}
+
+class CustomArrowBtn extends StatelessWidget {
+  const CustomArrowBtn({super.key, this.onPressed});
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadiusGeometry.circular(16),
+        ),
+        backgroundColor: AppColors.kPrimaryColor,
+        foregroundColor: AppColors.kWhiteColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        child: Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+}
+
+class CustomTextBtn extends StatelessWidget {
+  const CustomTextBtn({super.key, required this.text, this.onTap});
+  final String text;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: AppColors.kPrimaryColor,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
@@ -86,6 +174,7 @@ class CustomTextField extends StatelessWidget {
       keyboardType: keyboardType,
       obscureText: isHidden,
       decoration: InputDecoration(
+        labelStyle: TextStyle(color: AppColors.kBlackColor),
         labelText: label,
         suffixIcon: sufIcon,
         enabledBorder: OutlineInputBorder(
