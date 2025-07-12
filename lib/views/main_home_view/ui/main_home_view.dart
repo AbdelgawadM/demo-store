8685fa37829be731968c9b8c458bb2bde1a41327
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-12 04:28:25 +0300

    customize buttomnav bar

--- lib/views/main_home_view/ui/main_home_view.dart
+++ lib/views/main_home_view/ui/main_home_view.dart
@@ -3,7 +3,6 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:our_store/core/app_colors.dart';
 
-
 class MainHomeView extends StatefulWidget {
   const MainHomeView({super.key});
 
@@ -12,27 +11,24 @@ class MainHomeView extends StatefulWidget {
 }
 
 class _MainHomeViewState extends State<MainHomeView> {
-    int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(child: Text('data'),)),
+      body: SafeArea(child: Center(child: Text('data'))),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
           color: Colors.white,
           boxShadow: [
-            BoxShadow(
-              blurRadius: 20,
-              color: Colors.black.withOpacity(.1),
-            )
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1)),
           ],
         ),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
             child: GNav(
-              rippleColor:Colors.grey[300]!,
+              rippleColor: Colors.grey[300]!,
               hoverColor: Colors.grey[100]!,
               gap: 8,
               activeColor: AppColors.kWhiteColor,
@@ -40,24 +36,12 @@ class _MainHomeViewState extends State<MainHomeView> {
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
               duration: Duration(milliseconds: 400),
               tabBackgroundColor: AppColors.kPrimaryColor,
-              color: Colors.black,
+              color: Colors.black26,
               tabs: [
-                GButton(
-                  icon: LineIcons.home,
-                  text: 'Home',
-                ),
-                GButton(
-                  icon: LineIcons.heart,
-                  text: 'Likes',
-                ),
-                GButton(
-                  icon: LineIcons.search,
-                  text: 'Search',
-                ),
-                GButton(
-                  icon: LineIcons.user,
-                  text: 'Profile',
-                ),
+                GButton(icon: Icons.home, text: 'Home'),
+                GButton(icon: Icons.store, text: 'Likes'),
+                GButton(icon: Icons.favorite, text: 'Search'),
+                GButton(icon: Icons.person, text: 'Profile'),
               ],
               selectedIndex: _selectedIndex,
               onTabChange: (index) {
