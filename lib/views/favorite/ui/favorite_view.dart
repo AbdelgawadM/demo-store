ec1e54f0ba6f2e0fb4e3067ee276c348dfb74d33
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-13 07:26:15 +0300

    finish favorite view ui

--- lib/views/favorite/ui/favorite_view.dart
+++ lib/views/favorite/ui/favorite_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:our_store/core/widgets/custom_list.dart';
+import 'package:our_store/views/auth/ui/widgets/title_txt_field.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('favorite');
+    return ListView(children: [
+      Center(child: TitleTxtField(text: 'Your Favorite Products')),
+     CustomList()
+    ],);
   }
 }
