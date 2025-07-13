047dbf1844e364bb90a3c7860d3b9c6323dbf02c
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-13 06:55:19 +0300

    make custom card get padding and margin

--- lib/views/auth/ui/widgets/custom_card.dart
+++ lib/views/auth/ui/widgets/custom_card.dart
@@ -1,20 +1,20 @@
-
 import 'package:flutter/material.dart';
 import 'package:our_store/core/app_colors.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({super.key, this.child});
+  const CustomCard({super.key, this.child, this.margin, this.padding});
   final Widget? child;
-
+  final EdgeInsetsGeometry? margin;
+  final EdgeInsetsGeometry? padding;
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(24),
+      margin: margin ?? EdgeInsets.all(24),
       color: AppColors.kWhiteColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadiusGeometry.circular(16),
       ),
-      child: Padding(padding: const EdgeInsets.all(16.0), child: child),
+      child: Padding(padding: padding ?? EdgeInsets.all(16), child: child),
     );
   }
 }
