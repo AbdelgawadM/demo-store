3d8a6f437832bea063be298d752f67b4f4160555
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-15 06:59:43 +0300

    finish ptofile view ui with refactoring

--- lib/views/profile/ui/profile_view.dart
+++ lib/views/profile/ui/profile_view.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:our_store/core/app_colors.dart';
+import 'package:our_store/views/auth/ui/widgets/custom_card.dart';
+import 'package:our_store/views/profile/ui/widgets/custom_title.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('profile');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomCard(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: AppColors.kPrimaryColor,
+                foregroundColor: AppColors.kWhiteColor,
+                radius: 60,
+                child: Icon(Icons.person, size: 50),
+              ),
+              SizedBox(height: 15),
+              Text('My Name', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              Text(
+                'email@gmail.com',
+                style: TextStyle(
+                  color: AppColors.kGreyColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 15),
+
+              CustomTile(title: 'Edit Name', icon: Icons.person, onTap: () {}),
+              SizedBox(height: 20),
+              CustomTile(
+                title: 'My Orders',
+                icon: Icons.shopping_basket,
+                onTap: () {},
+              ),
+              SizedBox(height: 20),
+              CustomTile(title: 'Log Out', icon: Icons.logout, onTap: () {}),
+              SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
---
+++ lib/views/profile/ui/widgets/custom_title.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:our_store/core/app_colors.dart';
+
+class CustomTile extends StatelessWidget {
+  const CustomTile({
+    super.key,
+    this.onTap,
+    required this.title,
+    required this.icon,
+  });
+  final void Function()? onTap;
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(12),
+      ),
+      leading: Icon(icon),
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      trailing: Icon(Icons.arrow_forward_ios),
+      minLeadingWidth: 90,
+      textColor: AppColors.kWhiteColor,
+      tileColor: AppColors.kPrimaryColor,
+      iconColor: AppColors.kWhiteColor,
+      onTap: onTap,
+    );
+  }
+}
