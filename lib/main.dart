4e7ad4ed7c403fb9dd6d6483c70347c5cd0658e3
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-19 03:46:58 +0300

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:our_store/core/app_colors.dart';
+import 'package:our_store/secret_constants.dart';
+import 'package:our_store/views/auth/logic/cubit/auth_cubit.dart';
 import 'package:our_store/views/auth/ui/forget_view.dart';
 import 'package:our_store/views/auth/ui/login_view.dart';
 import 'package:our_store/views/auth/ui/sign_up_view.dart';
 import 'package:our_store/views/main_home_view/ui/main_home_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(url: url, anonKey: apiKey);
   runApp(const DemoStore());
 }
 
@@ -14,19 +20,22 @@ class DemoStore extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Demo Store',
-      theme: ThemeData(scaffoldBackgroundColor: AppColors.kScaffoldColor,
-      appBarTheme: AppBarTheme(
-        // color: AppColors.kPrimaryColor,
-        centerTitle: true,
-        backgroundColor: AppColors.kPrimaryColor,
-        foregroundColor: AppColors.kWhiteColor,
-        
-      )
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Demo Store',
+        theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.kScaffoldColor,
+          appBarTheme: AppBarTheme(
+            // color: AppColors.kPrimaryColor,
+            centerTitle: true,
+            backgroundColor: AppColors.kPrimaryColor,
+            foregroundColor: AppColors.kWhiteColor,
+          ),
+        ),
+        home: MainHomeView(),
       ),
-      home: MainHomeView(),
     );
   }
 }
