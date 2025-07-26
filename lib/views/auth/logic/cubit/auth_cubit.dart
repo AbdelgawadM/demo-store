712eaa498860bdd791d6515b0e285b821c853663
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-07-26 17:52:52 +0300

    add some auth methods to cupit, separate States

--- lib/views/auth/logic/cubit/auth_cubit.dart
+++ lib/views/auth/logic/cubit/auth_cubit.dart
@@ -11,35 +11,80 @@ class AuthCubit extends Cubit<AuthCubitState> {
 
   Future<void> login({required String email, required String password}) async {
     try {
-      emit(Loading());
+      emit(LoadingLogin());
       await client.auth.signInWithPassword(email: email, password: password);
-      emit(Success());
+      emit(SuccessLogin());
     } on AuthException catch (e) {
-      emit(Failure(message: e.toString()));
+      emit(FailureLogin(message: e.toString()));
     } catch (e) {
-      emit(Failure(message: e.toString()));
+      emit(FailureLogin(message: e.toString()));
     }
   }
 
   Future<void> signUp({required String email, required String password}) async {
     try {
-      emit(Loading());
+      emit(LoadingSignUp());
       await client.auth.signUp(email: email, password: password);
-      emit(Success());
+      emit(SuccessSignUp());
     } on AuthException catch (e) {
-      emit(Failure(message: e.toString()));
+      emit(FailureSignUp(message: e.toString()));
     } catch (e) {
-      emit(Failure(message: e.toString()));
+      emit(FailureSignUp(message: e.toString()));
     }
+  }
+
+  Future<void> sendEmail({required String email}) async {
+    try {
+      emit(LoadingEmailSent());
+      await client.auth.resetPasswordForEmail(
+        email,
+        redirectTo: 'ourstore://reset-password',
+      );
+      emit(SuccessEmailSent());
+    } catch (e) {
+      emit(FailureEmailSent(message: e.toString()));
+    }
+  }
+
+  Future<void> verifyOTP({
+    required String email,
+    required String otpCode,
+  }) async {
+    try {
+      emit(LoadingUpdating());
+      final session = await client.auth.verifyOTP(
+        type: OtpType.recovery,
+        email: email,
+        token: otpCode,
+      );
+
+      if (session.user != null) {
+        // Success! User is now signed in temporarily
+        // Navigate to a "Reset Password" screen
+        emit(SuccessVerfying());
+      }
+    } catch (e) {
+      emit(FailureVerfying(message: e.toString()));
+    }
+  }
+
+  Future<void> updatePassword(String password) async {
+    try {
+      emit(LoadingUpdating());
+      await client.auth.updateUser(UserAttributes(password: password));
+    } catch (e) {
+      emit(FailureUpdating(message: e.toString()));
+    }
+    emit(SuccessUpdating());
   }
 
   Future<void> logOut() async {
     try {
-      emit(Loading());
+      emit(LoadingLogout());
       await client.auth.signOut();
     } catch (e) {
-      emit(Failure(message: e.toString()));
+      emit(FailureLogout(message: e.toString()));
     }
-    emit(Success());
+    emit(SuccessLogout());
   }
 }
--- lib/views/auth/logic/cubit/auth_cubit_state.dart
+++ lib/views/auth/logic/cubit/auth_cubit_state.dart
@@ -1,14 +1,60 @@
 abstract class AuthCubitState {}
 
 final class AuthCubitInitial extends AuthCubitState {}
-
-final class Success extends AuthCubitState {}
-final class Failure extends AuthCubitState {
+final class SuccessLogin extends AuthCubitState {}
+final class FailureLogin extends AuthCubitState {
   final String message;
 
-  Failure({required this.message});
+  FailureLogin({required this.message});
 }
-final class Loading extends AuthCubitState {}
+final class LoadingLogin extends AuthCubitState {}
 
-// final class SuccessSignUp extends AuthCubitState {}
-// final class FailureSignUp extends AuthCubitState {}
+
+
+
+
+final class SuccessSignUp extends AuthCubitState {}
+final class FailureSignUp extends AuthCubitState {
+  final String message;
+
+  FailureSignUp({required this.message});
+}
+final class LoadingSignUp extends AuthCubitState {}
+
+
+final class SuccessEmailSent extends AuthCubitState {}
+final class FailureEmailSent extends AuthCubitState {
+  final String message;
+
+  FailureEmailSent({required this.message});
+}
+final class LoadingEmailSent extends AuthCubitState {}
+
+
+final class SuccessVerfying extends AuthCubitState {}
+final class FailureVerfying extends AuthCubitState {
+  final String message;
+
+  FailureVerfying({required this.message});
+}
+final class LoadingVerfying extends AuthCubitState {}
+
+
+final class SuccessUpdating extends AuthCubitState {}
+final class FailureUpdating extends AuthCubitState {
+  final String message;
+
+  FailureUpdating({required this.message});
+}
+final class LoadingUpdating extends AuthCubitState {}
+
+
+
+
+final class SuccessLogout extends AuthCubitState {}
+final class FailureLogout extends AuthCubitState {
+  final String message;
+
+  FailureLogout({required this.message});
+}
+final class LoadingLogout extends AuthCubitState {}
