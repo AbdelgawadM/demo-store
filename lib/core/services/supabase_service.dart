ce7ad147fd07496ec55d8cafcc6b0706e13937d9
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-08-06 02:14:30 +0300

    unique categories

--- lib/core/services/supabase_service.dart
+++ lib/core/services/supabase_service.dart
@@ -1,46 +1,40 @@
 import 'package:dio/dio.dart';
-import 'package:our_store/core/models/product_model.dart';
+
 import 'package:our_store/secret_constants.dart';
 import 'package:our_store/views/home/logic/models/category_model.dart';
 import 'package:our_store/views/home/logic/models/discounts_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
   final Dio _dio = Dio();
 
   final String supabaseUrl = url;
   final String supabaseKey = apiKey;
+  SupabaseClient supabase=Supabase.instance.client;
 
-  Future<List<CategoryModel>> fetchCategories() async {
-    final url = '$supabaseUrl/rest/v1/products?select=category';
+ Future<List<CategoryModel>> fetchCategories() async {
+  try {
+    final List<dynamic> response =
+        await supabase.rpc('get_unique_categories') as List<dynamic>;
 
-    final response = await _dio.get(
-      url,
-      options: Options(
-        headers: {
-          'apikey': supabaseKey,
-          'Authorization': 'Bearer $supabaseKey',
-        },
-      ),
-    );
-    try {
-      List dynamicList = response.data;
-      List<CategoryModel> modelList = (dynamicList)
-          .map((e) => CategoryModel.fromJson(e as Map<String, dynamic>))
-          .toList();
-      if (response.statusCode == 200) {
-        print('Fetched products:');
-        print(response.data);
-      } else {
-        print('Failed with status code: ${response.statusCode}');
-      }
-      return modelList;
-    } catch (e) {
-      throw Exception(e);
-    }
+    List<CategoryModel> modelList = response
+        .map((e) => CategoryModel(label: e.toString()))
+        .toList();
+
+    print('Fetched categories: $modelList');
+
+    return modelList;
+  } catch (e) {
+    throw Exception('Error fetching categories: $e');
   }
+}
 
-  Future<List<DiscountModel>> fetchDiscounts() async {
-    final url = '$supabaseUrl/rest/v1/discounts?select=*,products:for_product(*)';
+
+  Future<List<DiscountModel>> fetchDiscounts({required String parametersValue}) async {
+
+    
+    final url =
+        '$supabaseUrl/rest/v1/discounts?select=*,products:for_product($parametersValue)';
 
     final response = await _dio.get(
       url,
@@ -65,7 +59,38 @@ class SupabaseService {
       }
       return discountModelList;
     } catch (e) {
+      print(e.toString());
       throw Exception(e.toString());
     }
   }
+
+  // Future<List<ProductModel>> fetchProducts() async {
+  //   final url = '$supabaseUrl/rest/v1/products';
+
+  //   final response = await _dio.get(
+  //     url,
+  //     options: Options(
+  //       headers: {
+  //         'apikey': supabaseKey,
+  //         'Authorization': 'Bearer $supabaseKey',
+  //       },
+  //     ),
+  //   );
+
+  //   try {
+  //     List dynamicList = response.data;
+  //     List<DiscountModel> discountModelList = (dynamicList)
+  //         .map((e) => DiscountModel.fromJson(e as Map<String, dynamic>))
+  //         .toList();
+  //     if (response.statusCode == 200) {
+  //       print('Fetched products:');
+  //       print(response.data);
+  //     } else {
+  //       print('Failed with status code: ${response.statusCode}');
+  //     }
+  //     return discountModelList;
+  //   } catch (e) {
+  //     throw Exception(e.toString());
+  //   }
+  // }
 }
