6a078637b6a57779aaac00a9f81f2b308bcb1553
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-08-10 03:44:04 +0300

    make loading in new screen , clean builders

--- lib/core/widgets/product_builder.dart
+++ lib/core/widgets/product_builder.dart
@@ -1,60 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:our_store/core/cubits/product_details_cubit/product_details_cubit.dart';
-import 'package:our_store/core/cubits/product_details_cubit/product_details_state.dart';
 import 'package:our_store/core/functions/navigate_to.dart';
-import 'package:our_store/core/models/product_details_model.dart';
 import 'package:our_store/core/models/product_view_model.dart';
-import 'package:our_store/core/widgets/custom_indicator.dart';
 import 'package:our_store/views/home/ui/widgets/product.dart';
-
 import 'package:our_store/views/product_details/ui/products_details_view.dart';
 
 class ProductBuilder extends StatelessWidget {
   const ProductBuilder({super.key, required this.productViewModel});
-
   final List<ProductViewModel> productViewModel;
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ProductDetailsCubit, ProductDetailsState>(
-      listener: (context, state) {
-        if (state is ProductDetailsSuccess) {
-          final ProductDetailsCubit cubit = context.read<ProductDetailsCubit>();
-          final List<ProductDetailsModel> productDetailsModel =
-              state.productDetailsModel;
-
-          final selected = cubit.selectedIndex;
-
+    return ListView.separated(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: productViewModel.length,
+      separatorBuilder: (context, index) => const SizedBox(height: 15),
+      itemBuilder: (context, index) => Product(
+        productViewModel: productViewModel[index],
+        onTap: () {
           navigateTo(
             context,
-            ProductsDetailsView(
-              productViewModel: productViewModel[selected!],
-              productDetailsModel: productDetailsModel.first,
-            ),
+            ProductsDetailsView(productViewModel: productViewModel[index]),
           );
-        }
-      },
-      builder: (context, state) {
-        return state is ProductDetailsLoading
-            ? const CustomIndicator()
-            : ListView.separated(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Product(
-                  productViewModel: productViewModel[index],
-                  onTap: () {
-                    final ProductDetailsCubit cubit = context
-                        .read<ProductDetailsCubit>();
-                    cubit.selectedIndex = index;
-                    cubit.getProductDetails(id: productViewModel[index].id);
-                  },
-                ),
-                separatorBuilder: (context, index) =>
-                    const SizedBox(height: 15),
-                itemCount: productViewModel.length,
-              );
-      },
+        },
+      ),
     );
   }
 }
--- lib/views/product_details/ui/products_details_view.dart
+++ lib/views/product_details/ui/products_details_view.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:our_store/core/models/product_details_model.dart';
+import 'package:our_store/core/cubits/product_details_cubit/product_details_cubit.dart';
+import 'package:our_store/core/cubits/product_details_cubit/product_details_state.dart';
 import 'package:our_store/core/models/product_view_model.dart';
+import 'package:our_store/core/widgets/custom_indicator.dart';
 import 'package:our_store/views/auth/ui/widgets/custom_text_field.dart';
 import 'package:our_store/views/home/ui/widgets/custom_cashed_image.dart';
 
-class ProductsDetailsView extends StatelessWidget {
-  const ProductsDetailsView({
-    super.key,
-    required this.productViewModel,
-    required this.productDetailsModel,
-  });
+class ProductsDetailsView extends StatefulWidget {
+  const ProductsDetailsView({super.key, required this.productViewModel});
   final ProductViewModel productViewModel;
-  final ProductDetailsModel productDetailsModel;
+
+  @override
+  State<ProductsDetailsView> createState() => _ProductsDetailsViewState();
+}
+
+class _ProductsDetailsViewState extends State<ProductsDetailsView> {
+  late ProductDetailsCubit productDetailsCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    productDetailsCubit = context.read<ProductDetailsCubit>()
+      ..getProductDetails(id: widget.productViewModel.id);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(productViewModel.name),
+        title: Text(widget.productViewModel.name),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -26,57 +38,67 @@ class ProductsDetailsView extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: ListView(
-        children: [
-          CustomCashedImage(imageUrl: productViewModel.imageUrl),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 24,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              spacing: 20,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+        builder: (context, state) {
+          return state is ProductDetailsSuccess
+              ? ListView(
                   children: [
-                    Text(productViewModel.name),
-                    Text(productViewModel.price.toString()),
-                  ],
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Text('0 '),
-                        Icon(Icons.star, color: Colors.amberAccent),
-                      ],
+                    CustomCashedImage(
+                      imageUrl: widget.productViewModel.imageUrl,
                     ),
-                    Icon(Icons.favorite),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 24,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        spacing: 20,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(widget.productViewModel.name),
+                              Text(widget.productViewModel.price.toString()),
+                            ],
+                          ),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Text('0 '),
+                                  Icon(Icons.star, color: Colors.amberAccent),
+                                ],
+                              ),
+                              Icon(Icons.favorite),
+                            ],
+                          ),
+                          Text(state.productDetailsModel.first.description),
+                          RatingBar.builder(
+                            initialRating: 3,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemPadding: const EdgeInsets.symmetric(
+                              horizontal: 4.0,
+                            ),
+                            itemBuilder: (context, _) =>
+                                const Icon(Icons.star, color: Colors.amber),
+                            onRatingUpdate: (rating) {},
+                          ),
+                          const CustomTextField(
+                            keyboardType: TextInputType.multiline,
+                            hint: Text('comment here...'),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
-                ),
-                Text(productDetailsModel.description),
-                RatingBar.builder(
-                  initialRating: 3,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder: (context, _) =>
-                      const Icon(Icons.star, color: Colors.amber),
-                  onRatingUpdate: (rating) {},
-                ),
-                const CustomTextField(
-                  keyboardType: TextInputType.multiline,
-                  hint: Text('comment here...'),
-                ),
-              ],
-            ),
-          ),
-        ],
+                )
+              : const CustomIndicator();
+        },
       ),
     );
   }
