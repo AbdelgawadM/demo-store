89aff826f3f70aa0c8066b84c919520c29a8c95f
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-08-15 11:05:19 +0300

    remove and repair

--- lib/core/models/product_details_model.dart
+++
@@ -1,8 +0,0 @@
-class ProductDetailsModel {
-  final String description;
-
-  ProductDetailsModel({required this.description});
-  factory ProductDetailsModel.fromJson(Map<String, dynamic> json) {
-    return ProductDetailsModel(description: json['description']);
-  }
-}
