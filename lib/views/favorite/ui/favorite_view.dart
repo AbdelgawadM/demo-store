5c08d9d1d1ece6c896843224c7c3cbafd9d4484a
Abdelgawad Mahmoud <abdelgawadmahmoud333@gmail.com>
2025-08-17 10:35:01 +0300

    make initialization out of init state

--- lib/views/favorite/ui/favorite_view.dart
+++ lib/views/favorite/ui/favorite_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:our_store/core/cubits/favorite_cubit/favorite_cubit.dart';
+import 'package:our_store/core/functions/custom_snackbar.dart';
 import 'package:our_store/core/widgets/custom_indicator.dart';
 import 'package:our_store/core/widgets/product_builder.dart';
 import 'package:our_store/views/auth/logic/cubit/auth_cubit.dart';
@@ -14,37 +15,52 @@ class FavoriteView extends StatefulWidget {
 }
 
 class _FavoriteViewState extends State<FavoriteView> {
-  late FavoriteCubit favoriteCubit;
+  late FavoriteCubit favoriteCubit = context.read<FavoriteCubit>();
+  late String userId = context.read<AuthCubit>().client.auth.currentUser!.id;
 
   @override
   void initState() {
     super.initState();
-    AuthCubit authCubit = context.read<AuthCubit>();
-    favoriteCubit = context.read<FavoriteCubit>()
-      ..getFavorites(userId: authCubit.client.auth.currentUser!.id);
+    favoriteCubit.getFavorites(userId: userId);
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        const Center(child: TitleTxtField(text: 'Your Favorite Products')),
-        BlocBuilder<FavoriteCubit, FavoriteState>(
-          builder: (context, state) {
-            if (state is FavoriteLoading) {
-              return const CustomIndicator();
-            } else if (state is FavoriteSuccess) {
-              return ProductBuilder(
-                productViewModel: state.favoriteModel
-                    .map((fav) => fav.productViewModel)
-                    .toList(),
-              );
-            } else {
-              return const Center(child: Text('Error has Ocurred'));
-            }
-          },
-        ),
-      ],
+    return RefreshIndicator(
+      onRefresh: () async {
+        await favoriteCubit.refresh(userId: userId);
+      },
+      child: ListView(
+        children: [
+          const Center(child: TitleTxtField(text: 'Your Favorite Products')),
+          BlocConsumer<FavoriteCubit, FavoriteState>(
+            listener: (context, state) {
+              if (state is FavoriteAddedFaliure) {
+                customSnackBar(
+                  context,
+                  'favourite not added: ${state.message}',
+                );
+              }
+              if (state is FavoriteFaliure) {
+                customSnackBar(context, state.message);
+              }
+            },
+            builder: (context, state) {
+              if (state is FavoriteLoading) {
+                return const CustomIndicator();
+              } else if (state is FavoriteSuccess) {
+                return ProductBuilder(
+                  productViewModel: state.favoriteModel
+                      .map((fav) => fav.productViewModel)
+                      .toList(),
+                );
+              } else {
+                return const Center(child: Text('Error has Ocurred'));
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
